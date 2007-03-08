eccedde9ea7fe6638bfcf49bbd56eb4afac350a3
Mark Bergsma <Mark Bergsma@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2007-03-08 18:39:01 +0000

    Move main function file outside the package
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/pybal@20260 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+"""
+PyBal
+Copyright (C) 2006 by Mark Bergsma <mark@nedworks.org>
+
+LVS Squid balancer/monitor for managing the Wikimedia Squid servers using LVS
+"""
+
+from pybal import pybal
+
+if __name__ == '__main__':
+    pybal.main()
