1d2a4d3303ffa3cf1cc220c08eb92d5dd23159d3
Mark Bergsma <mark@wikimedia.org>
2007-03-08 16:58:23 +0000

    * Support Python DistUtils * Update TODO
    git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/pybal@20245 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

---
+++ pybal/__init__.py
@@ -0,0 +1,8 @@
+"""
+pybal.__init__.py
+Copyright (C) 2006 by Mark Bergsma <mark@nedworks.org>
+
+The pybal package contains all PyBal modules
+"""
+
+__all__ = [ 'ipvs', 'monitor', 'pybal', 'util', 'monitors' ]
