8a14dcb49a4fa6fdf971095177da4155e68f6323
mark <mark@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-08-10 23:26:38 +0000

    - New upstream version with slipstreamed twistedbgp - Fix initscript to not start duplicate processes
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/pybal@39108 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- pybal/__init__.py
+++ pybal/__init__.py
@@ -5,4 +5,4 @@ Copyright (C) 2006 by Mark Bergsma <mark@nedworks.org>
 The pybal package contains all PyBal modules
 """
 
-__all__ = [ 'ipvs', 'monitor', 'pybal', 'util', 'monitors' ]
+__all__ = [ 'ipvs', 'monitor', 'pybal', 'util', 'monitors', 'bgp' ]
