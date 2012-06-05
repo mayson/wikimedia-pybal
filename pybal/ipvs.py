6106ee393069dd7a87916001e8e138788835309f
Mark Bergsma <mark@wikimedia.org>
2012-06-05 10:07:19 +0200

    Make sure we don't pass any IPv6 service IP addresses to BGP at the moment

--- pybal/ipvs.py
+++ pybal/ipvs.py
@@ -176,7 +176,7 @@ class LVSService:
 
         self.ipvsManager.DryRun = configuration.getboolean('dryrun', False)
         
-        if self.configuration.getboolean('bgp', False):
+        if self.configuration.getboolean('bgp', False) and ':' not in self.ip:
             from pybal import BGPFailover
             # Add service ip to the BGP announcements
             BGPFailover.addPrefix(self.ip)
