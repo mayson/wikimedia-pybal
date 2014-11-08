f51a6c98f463389d23af2b6bbad1959aef66105f
Ori Livneh <ori@wikimedia.org>
2014-11-08 14:36:25 -0800

    Add unit tests for `pybal.util.LogFile`
    Easy.
    
    Change-Id: I1d75808a50f20dba6c8b0ef9f3b674eb3a8b9a66

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,9 +7,52 @@
 
 """
 import unittest
+import tempfile
+import os
 
 import pybal
 import pybal.util
+
+
+class LogFileTestCase(unittest.TestCase):
+    """Test case for `pybal.util.LogFile`."""
+
+    TIMESTAMP_REGEXP = r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+'
+
+    @classmethod
+    def setUpClass(cls):
+        file_handle, cls.path = tempfile.mkstemp('.pybal.test.log')
+        os.close(file_handle)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.unlink(cls.path)
+
+    def setUp(self):
+        self.log_file = pybal.util.LogFile(self.path)
+
+    def tearDown(self):
+        self.log_file.file.close()
+
+    def testWrite(self):
+        """Test `LogFile.write`."""
+        log_lines = ('line 1\n', 'line 2\n', 'line 3\n')
+        self.log_file.write(''.join(log_lines))
+        with open(self.path, 'r') as f:
+            file_lines = f.readlines()
+        self.assertEqual(len(file_lines), 3)
+        for log_line, file_line in zip(log_lines, file_lines):
+            self.assertIn(log_line, file_line)
+            self.assertRegexpMatches(file_line, self.TIMESTAMP_REGEXP)
+
+    def testReopen(self):
+        """Test `LogFile.reopen`."""
+        os.unlink(self.path)
+        self.log_file.reopen()
+        self.log_file.write('test')
+        self.assertTrue(os.path.exists(self.path))
+        with open(self.path, 'r') as f:
+            self.assertIn('test', f.read())
 
 
 class ConfigDictTestCase(unittest.TestCase):
