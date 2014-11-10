baf652a0eac0276f9957de04616a2feab4810991
Ori Livneh <ori@wikimedia.org>
2014-11-10 09:58:39 -0800

    Re-add explicit test loader in tests/__init__.py
    tox autodiscovers the tests, but a plain 'python setup.py test' does not.
    
    Change-Id: I9bd45f4108c7e0e39328fbd487e8331abaf19b46

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+  PyBal unit tests
+  ~~~~~~~~~~~~~~~~
+
+  This module is the main entry point for the unit tests.
+
+"""
+# flake8: noqa
+
+from .test_util import *
+from .test_ipvs import *
