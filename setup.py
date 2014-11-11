7c4c25ef1722a2bc0a7fecc0b99702bd22f17e5e
Antoine Musso <hashar@free.fr>
2014-11-11 23:08:24 +0100

    Lint setup.py
    The first parameter of the setup() call is now on its online just like
    the others.
    
    Change-Id: I523ba9391791b84b9c0b336ca875318df23d8455

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ except ImportError:
     from distutils.core import setup
 
 
-setup(name="pybal",
+setup(
+    name="pybal",
     version="0.1",
     description="PyBal LVS monitor",
     author="Mark Bergsma",
