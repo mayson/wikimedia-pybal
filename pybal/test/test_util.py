2de67bc48e46841d23739b8f5365a93eac22986e
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-09-18 13:49:16 +0200

    Remove LogFile
    It is not used anymore, and we probably want to use python's own logging
    in the future if we need to.
    
    Change-Id: I7af629bc1be4867e3dbe0b58e00d10cef2d45a14

--- pybal/test/test_util.py
+++ pybal/test/test_util.py
@@ -16,42 +16,6 @@ import pybal.util
 from .fixtures import PyBalTestCase
 
 
-class LogFileTestCase(PyBalTestCase):
-    """Test case for `pybal.util.LogFile`."""
-
-    TIMESTAMP_REGEXP = r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+'
-
-    def setUp(self):
-        super(LogFileTestCase, self).setUp()
-        file_handle, self.path = tempfile.mkstemp('.pybal.test.log')
-        os.close(file_handle)
-        self.log_file = pybal.util.LogFile(self.path)
-
-    def tearDown(self):
-        self.log_file.file.close()
-        os.unlink(self.path)
-
-    def testWrite(self):
-        """Test `LogFile.write`."""
-        log_lines = ('line 1\n', 'line 2\n', 'line 3\n')
-        self.log_file.write(''.join(log_lines))
-        with open(self.path, 'r') as f:
-            file_lines = f.readlines()
-        self.assertEqual(len(file_lines), 3)
-        for log_line, file_line in zip(log_lines, file_lines):
-            self.assertIn(log_line, file_line)
-            self.assertRegexpMatches(file_line, self.TIMESTAMP_REGEXP)
-
-    def testReopen(self):
-        """Test `LogFile.reopen`."""
-        os.unlink(self.path)
-        self.log_file.reopen()
-        self.log_file.write('test')
-        self.assertTrue(os.path.exists(self.path))
-        with open(self.path, 'r') as f:
-            self.assertIn('test', f.read())
-
-
 class MiscUtilTestCase(PyBalTestCase):
     """Test case for misc. methods in `pybal.util`."""
 
--- pybal/util.py
+++ pybal/util.py
@@ -17,35 +17,6 @@ def get_subclasses(cls):
     return subclasses
 
 
-class LogFile(object):
-    def __init__(self, filename):
-        self.filename = filename
-        self.lineEnded = True
-        self.file = file(filename, 'a')
-
-    def write(self, s):
-        """
-        Write string to logfile with a timestamp
-        """
-        lines = s.splitlines(True)
-        for line in lines:
-            if self.lineEnded:
-                timestamp = datetime.datetime.now()
-                self.file.write(str(timestamp) + ' ')
-            self.file.write(line)
-            self.lineEnded = line.endswith('\n')
-
-        self.file.flush()
-
-    def reopen(self):
-        """
-        Close the logfile and reopen it. Useful for log rotation.
-        """
-        self.file.close()
-        self.file = file(self.filename, 'a')
-        self.lineEnded = True
-
-
 class ConfigDict(dict):
 
     def getint(self, key, default=None):
